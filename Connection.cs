1376cabb00fbbc1c2ce93e87e59f5132c98afd18
Elissandro Vieira <teofiloeli@gmail.com>
2024-03-19 16:19:43 -0300

    Add Connection.

---
+++ Connection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace EmpresaZ.Dynamics365.Query
+{
+    public class Connection
+    {
+
+        public static IOrganizationService Service { get; set; }
+
+        public static IOrganizationService GetService()
+        {
+            if (Service == null) {
+
+                var url = "https://org9a10be3e.crm2.dynamics.com";
+                var user = "ElissandroVieira@TeofiloVieira.onmicrosoft.com";
+                var password = "Luanavieir4.";
+
+                CrmServiceClient crmServiceClient = new CrmServiceClient(
+                    "AuthType=Office365;" +
+                    $"Url={url};" +
+                    $"Username={user};" +
+                    $"Password={password}"
+                );
+
+                Service = crmServiceClient.OrganizationWebProxyClient;
+            }
+            
+
+            return Service;
+        }
+
+    }
+}
